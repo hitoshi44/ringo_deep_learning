c061d8d6b0d1220e00321f2963fc25bad784a75e
Hitoshi Harumi <hrmhts0404@gmail.com>
2023-01-13 14:35:26 +0900

    learn_mse_loss.py 追加

---
+++ learn_mse_loss.py
@@ -0,0 +1,31 @@
+import numpy as np
+from mods import mnist, network
+
+(images, labels) = mnist.np_load_train()
+NN = network.NeuralNetwork((784,100,10), network.mse_loss)
+
+label = np.zeros(10)
+i = 0
+size = len(images)
+chunk = range(50)
+while i < size:
+    for _ in chunk:
+        label.fill(0)
+        label[labels[i]] = 1.0
+        output = NN.forward(images[i])
+        delta = NN.calc_loss(output, label)
+        NN.backward( delta )
+        i+=1
+    
+    if i % 1000 == 0:
+        print("At ", i, " Loss: ", NN.Loss)
+    NN.update(0.01)
+
+(tests, labels) = mnist.np_load_train()
+hit = 0
+for (image, label) in zip(tests, labels):
+    pred = np.argmax(NN.fire(image))
+    if pred == label:
+        hit += 1
+print("Accuracy :", hit/len(tests))
+print("  ", hit, " / ", len(tests))
